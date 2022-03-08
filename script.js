aa36ba38a5f797d662e3665e29d0810f90be0343
Langchao Wu <langchaowu@yahoo.com>
2022-03-08 12:07:24 -0500

    render pokemons types info

--- script.js
+++ script.js
@@ -19,16 +19,42 @@ function createPokemonCard(pokemon){
         const pokeCard=document.createElement("div")
         pokeCard.id=pokemon.id
         pokeCard.className='pokemon'
-
+        
+        
         const name=pokemon.name[0].toUpperCase()+pokemon.name.slice(1);
+        const types=[];
 
-        pokeCard.innerHTML=`
-            <div class='img-container'>
-                <img alt=${pokemon.name} src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png'>
-            </div>
+        for (const type in pokemon.types) {
+            
+            types.push(pokemon.types[type].type.name)
+        }
+        
+        const imgContainer=document.createElement("div")
+        imgContainer.className='img-container'
+        const pokeImg=document.createElement('img')
+        pokeImg.alt=pokemon.name
+        pokeImg.src=`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`
+        imgContainer.append(pokeImg)
 
-        `   
+        const pokeInfo=document.createElement("div")
+        pokeInfo.className="info"
+        const span=document.createElement("span")
+        span.textContent=pokemon.id
+        span.className="number"
+        const h3=document.createElement("h3")
+        h3.textContent=name;
 
+        pokeInfo.append(span,h3)
+
+        types.forEach( function(type){
+            const spanType=document.createElement("span")
+            spanType.className=`back-ground-color-${type}`
+            spanType.textContent=type
+            pokeInfo.appendChild(spanType)
+        })
+
+
+        pokeCard.append(imgContainer,pokeInfo)
 
         pokeContainer.append(pokeCard)
 
